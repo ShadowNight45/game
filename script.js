ff127a2890a0f04dd46dae3289a1f4ce290f699f
ShadowNight45 <153755157+ShadowNight45@users.noreply.github.com>
2023-12-18 17:58:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var bouton = document.getElementById('connecter');
+       
+    bouton.addEventListener('click', function() {
+        // Récupérer les valeurs des champs utilisateur et password au moment du clic
+         var utilisateur = document.getElementById('utilisateur').value;
+         var password = document.getElementById('password').value;
+    
+        if (utilisateur === "2023PaoloAimeHameed" && password === "2023PaoloEmbrasseHameed") {
+            window.location.href = 'https://www.filecrypt.cc/Container/650D711AAB.html';
+        } else {
+            alert("Mauvaise Combinaison");
+        }
+    });
+});
